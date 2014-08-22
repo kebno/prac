fea05c32219890e604a23e6cad1528522f8239f6
kebno <jboyle@pdx.edu>
2014-08-22 01:51:07 -0700

    bring back simple example

---
+++ 008/simple-passthrough.c
@@ -0,0 +1,18 @@
+/* PASTHROUGH.C
+ *  Aug 2014, John Boyle
+ *
+ *  Reads from stdin and writes it to stdout.
+ *
+ *  From tutorial at http://mishotips.blogspot.com/2013/05/redirecting-standard-input-and-output.html
+ */
+
+#include <stdio.h>
+int
+main()
+{
+	int c;
+	while ((c = getchar()) != EOF)
+		putchar(c);
+	
+	return 0;
+}
