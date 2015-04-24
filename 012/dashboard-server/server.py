bb2116de6822f2a9b565da39b294ac9012e3e022
kebno <jkboyle@ucsd.edu>
2015-04-23 21:39:54 -0700

    add new url, page updates peridically with jquery function

--- 012/dashboard-server/server.py
+++ 012/dashboard-server/server.py
@@ -1,7 +1,7 @@
 #!/bin/python
 
 from flask import Flask, render_template
-import random
+import random, subprocess
 
 app = Flask(__name__)
 
@@ -9,5 +9,10 @@ app = Flask(__name__)
 def hello_world():
 	return render_template('dashboard.html', N_images=random.randint(1,9999),mb_used=random.randint(1,9999),disk_percent_used=random.randint(0,99))
 
+@app.route('/date/')
+def get_time():
+    p = subprocess.Popen('date', shell=True, stdout=subprocess.PIPE,executable='/bin/bash')
+    return p.communicate()[0]
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+	app.run(debug=True,host='0.0.0.0')
